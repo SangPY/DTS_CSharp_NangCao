bc7b325db47f5aa72828dc175a12090cf792d14b
SangDoThanh <sangdothanh95@gmail.com>
2024-09-11 23:47:39 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DTS_CSharp_NangCap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
